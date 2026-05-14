b398af74434e40bea84c36249b9187acc60107b9
Farshad Aghasharif <110560112+ieeexplorer@users.noreply.github.com>
2026-05-14 08:29:25 +0100

    Add orchestration package init

---
+++ src/agentic_research_toolkit/orchestration/__init__.py
@@ -0,0 +1,3 @@
+from agentic_research_toolkit.orchestration.workflow import ResearchWorkflow, WorkflowResult, WorkflowStepResult
+
+__all__ = ["ResearchWorkflow", "WorkflowResult", "WorkflowStepResult"]
